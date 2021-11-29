9a2606c85e35aa3f8d24477a15204ab250a40296
Chr4st <84637278+Chr4st@users.noreply.github.com>
2021-11-29 21:30:02 +0100

    Update InvSave.php

--- InvSave/InvSave/InvSave.php
+++ InvSave/InvSave/InvSave.php
@@ -1,65 +1,64 @@
 <?php
 
-// namespace iNxtInvSave;
+ namespace InvSave;
 
-// use pocketmine\plugin\PluginBase;
+ use pocketmine\plugin\PluginBase;
 
-// use pocketmine\event\Listener;
+ use pocketmine\event\Listener;
 
-// use pocketmine\Player;
+ use pocketmine\Player;
 
-// use pocketmine\event\player\PlayerJoinEvent;
+ use pocketmine\event\player\PlayerJoinEvent;
 
-// use pocketmine\event\entity\EntityLevelChangeEvent;
+ use pocketmine\event\entity\EntityLevelChangeEvent;
 
-// use pocketmine\inventory\PlayerInventory;
+ use pocketmine\inventory\PlayerInventory;
 
-// use pocketmine\utils\Config;
+ use pocketmine\utils\Config;
 
-// class InvSave extends PluginBase implements Listener {
+ class InvSave extends PluginBase implements Listener {
 
-// 	public function onEnable() {
-// 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-// 		if(!is_dir($this->getDataFolder() . "players/")) {
-// 			@mkdir($this->getDataFolder() . "players/");
-// 		}
-// 	}
-
-// 	public function onJoin(PlayerJoinEvent $event) {
-// 		$player = $event->getPlayer();
-// 		if(!file_exists($this->getDataFolder() ."players/" . $player->getName() . ".yml")) {
-// 			$this->inv = new Config($this->getDataFolder()."MS/" . $player->getName() . ".yml", CONFIG::YAML);
-// 			$this->inv->set("Inventory");
-// 			$this->inv->save();
-// 		}
-// 	}
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		if(!is_dir($this->getDataFolder() . "players/")) {
+			@mkdir($this->getDataFolder() . "players/");
+	}
+	}
+	public function onJoin(PlayerJoinEvent $event) {
+		$player = $event->getPlayer();
+ 		if(!file_exists($this->getDataFolder() ."players/" . $player->getName() . ".yml")) {
+			$this->inv = new Config($this->getDataFolder()."MS/" . $player->getName() . ".yml", CONFIG::YAML);
+			$this->inv->set("Inventory");
+			$this->inv->save();
+		}
+ 	}
 	
-// 	public function onLevelChange(EntityLevelChangeEvent $event) {
-// 		$entity = $event->getEntity();
-// 		if($entity instanceof Player){
-// 			$last = $event->getOrigin()->getName();
-// 			$new = $event->getTarget()->getName();
-// 			if($last == "MS") {
-// 				$this->save($entity);
-// 			}
-// 			if($new == "MS") {
-// 				$this->give($entity);
-// 			}
-// 		}
-// 	}
+	public function onLevelChange(EntityLevelChangeEvent $event) {
+ 		$entity = $event->getEntity();
+		if($entity instanceof Player){
+			$last = $event->getOrigin()->getName();
+			$new = $event->getTarget()->getName();
+			if($last == "MS") {
+				$this->save($entity);
+			}
+			if($new == "MS") {
+				$this->give($entity);
+			}
+	}
+ 	}
 	
-// 	public function save(Player $player) {
-// 		$items = $player->getInventory()->getContents(true);
-// 		$this->inv->set("Inventory", $items);
-// 		$this->inv->save();
-// 	}
+	public function save(Player $player) {
+ 		$items = $player->getInventory()->getContents(true);
+ 		$this->inv->set("Inventory", $items);
+		$this->inv->save();
+	}
 	
-// 	public function give(Player $player) {
-// 		$items = $this->inv->get("Inventory");
-// 		$contents = [];
-// 		foreach($items as $i) {
-// 			$contents[] = Item::get($i[0], $i[1], $i[2]);
-// 		}
-// 		$player->getInventory()->setContents($contents);
-// 	}
-// }
+	public function give(Player $player) {
+		$items = $this->inv->get("Inventory");
+		$contents = [];
+		foreach($items as $i) {
+ 			$contents[] = Item::get($i[0], $i[1], $i[2]);
+		}
+		$player->getInventory()->setContents($contents);
+ 	}
+ }
